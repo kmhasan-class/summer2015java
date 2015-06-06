5e4fb6fd3c64276bde026d2a77634c80f66e6590
kmhasan-class <kmhasan.class@gmail.com>
2015-06-06 21:49:55 +0600

    Update LoopTest.java
    Testing to see if edits from GitHub get reflected into netbeans projects

--- Loop Test/src/bd/ac/seu/looptest/LoopTest.java
+++ Loop Test/src/bd/ac/seu/looptest/LoopTest.java
@@ -16,7 +16,7 @@ public class LoopTest {
      */
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++)
-            System.out.println(i + "**");
+            System.out.println(i + "--");
     }
     
 }

e1e08a7faf4344360b7cb8c7a0610a96e4dc68a9
kmhasan <kmhasan@SEUPC0719>
2015-06-11 12:10:52 +0600

    Adding Product.java

--- POS Console Application/src/bd/ac/seu/pos/console/POSConsoleApplication.java
+++ POS Console Application/src/bd/ac/seu/pos/console/POSConsoleApplication.java
@@ -17,26 +17,12 @@ public class POSConsoleApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Product a = new Product(1001, "Matador Pen", "Stationery", 5.00);
-        Product b = new Product(2010, "Aarong Milk", "Dairy", 70.00);
-        Product c = new Product(5055, "Tomato", "Fresh produce", 45.00);
-        Customer c1 = new Customer(1020, "Md. Ashiqur Rahman", "0171111111");
-        Customer c2 = new Customer(1021, "Md. Rakibul Hasan", "0172211111");
-        Customer c3 = new Customer(1023, "Hasan Tareque", "0172233111");
-        Transaction t1 = new Transaction(1, LocalDate.now(), c1, b, 10);
-        System.out.printf("Product ID   Product name   Product type   Unit price\n");
-        System.out.printf("----------   ------------   ------------   ----------\n");
-        System.out.printf("%10d   %-12s   %-13s  %10.2f\n", a.getProductId(), a.getProductName(), a.getProductType(), a.getUnitPrice());
-        System.out.printf("%10d   %-12s   %-13s  %10.2f\n", b.getProductId(), b.getProductName(), b.getProductType(), b.getUnitPrice());
-        System.out.printf("%10d   %-12s   %-13s  %10.2f\n", c.getProductId(), c.getProductName(), c.getProductType(), c.getUnitPrice());
-
-        System.out.println("Transaction details of selling 10 liter aarong milk to Md. Ashiqur Rahman");
-        System.out.printf("Date: %s\n", t1.getTransactionDate());
-        System.out.printf("Customer name: %s\n", t1.getCustomer().getCustomerName());
-        System.out.printf("Product name: %s\n", t1.getProduct().getProductName());
-        System.out.printf("Product price: %.2f\n", t1.getProduct().getUnitPrice());
-        System.out.printf("Quantity bought: %.2f\n", t1.getQuantity());
-        System.out.printf("Total bill: %.2f\n", t1.getBill());
+        Product p1 = new Product(101, "Matador Pen", "Stationery", 5.50);
+        Product p2 = new Product(102, "Econo Pen", "Stationery", 7.00);
+        Product p3 = new Product(5001, "Aarong Milk", "Dairy", 75.00);
+        System.out.printf("%5d %-20s %-10s %5.2f\n", p1.getProductId(), p1.getProductName(), p1.getProductType(), p1.getUnitPrice());
+        System.out.printf("%5d %-20s %-10s %5.2f\n", p2.getProductId(), p2.getProductName(), p2.getProductType(), p2.getUnitPrice());
+        System.out.printf("%5d %-20s %-10s %5.2f\n", p3.getProductId(), p3.getProductName(), p3.getProductType(), p3.getUnitPrice());
     }
     
 }

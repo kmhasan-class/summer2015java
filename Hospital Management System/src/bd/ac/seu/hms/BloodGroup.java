2c65ccaa91a8eb1b731a5675439b8cbc30c00efa
kmhasan <kmhasan@SEUPC0719>
2015-06-25 11:12:23 +0600

    Hospital Management

---
+++ Hospital Management System/src/bd/ac/seu/hms/BloodGroup.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.ac.seu.hms;
+
+/**
+ *
+ * @author kmhasan
+ */
+public enum BloodGroup {
+    O_NEGATIVE,
+    O_POSITIVE,
+    A_NEGATIVE,
+    A_POSITIVE,
+    B_NEGATIVE,
+    B_POSITIVE,
+    AB_NEGATIVE,
+    AB_POSITIVE;
+}

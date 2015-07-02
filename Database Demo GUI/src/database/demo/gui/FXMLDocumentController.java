3bf7ef45b378c4b9d1ceeedfc050fc1eebdbf188
kmhasan <kmhasan@SEUPC0719>
2015-07-02 13:49:55 +0600

    DB GUI

--- Database Demo GUI/src/database/demo/gui/FXMLDocumentController.java
+++ Database Demo GUI/src/database/demo/gui/FXMLDocumentController.java
@@ -6,7 +6,13 @@
 package database.demo.gui;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -26,14 +32,38 @@ public class FXMLDocumentController implements Initializable {
     private TextField categoryField;
     @FXML
     private TextField unitPriceField;
+    Statement statement;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        String DB_URL = "jdbc:mysql://172.17.0.119/storedb";
+        String DB_USER = "cse2015";
+        String DB_PASS = "summer2015";
+        
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+            statement = connection.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
 
     @FXML
     private void handleAddProductAction(ActionEvent event) {
+        int id = Integer.parseInt(idField.getText());
+        String name = nameField.getText();
+        String category = categoryField.getText();
+        double unitPrice = Double.parseDouble(unitPriceField.getText());
+        Product p = new Product(id, name, category, unitPrice);
+        String query = "insert into Product values(" + p.getProductId() + ", "
+                + "'" + p.getProductName() + "', " 
+                + "'" + p.getCategory() + "', "
+                + p.getUnitPrice() + ")";
+        try {
+            statement.execute(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }

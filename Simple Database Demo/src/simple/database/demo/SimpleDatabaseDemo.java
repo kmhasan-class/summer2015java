2fa17a6e3edaa32b4cdd322546b39cdce95320bc
kmhasan <kmhasan@SEUPC0719>
2015-07-02 13:10:18 +0600

    Simple Database Demo

---
+++ Simple Database Demo/src/simple/database/demo/SimpleDatabaseDemo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple.database.demo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class SimpleDatabaseDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Product p = new Product(666, "Walton Primo NX", "Phone", 18000.00);
+    }
+    
+}
